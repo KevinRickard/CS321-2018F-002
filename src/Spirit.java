022bc74312dc65ac3883d45569d8b0a616c77e0e
ionic-atom <jdenton3@gmu.edu>
2018-11-18 19:30:52 -0500

    comments for Spirit

--- src/Spirit.java
+++ src/Spirit.java
@@ -1,7 +1,14 @@
+
+ /**
+ * @author Thaovy
+ * The Spirit class extends from NPC, they are roamers
+ * which can be caught by players.
+ */
 public class Spirit extends NPC {
     
     Spirits spiritType;
 
+    // Spirit constructor
     Spirit(GameCore gameCore, String name, int currentRoom, long aiPeriodSeconds, Spirits spiritType) {
         super(gameCore, name, currentRoom, aiPeriodSeconds);
         this.spiritType = spiritType;
